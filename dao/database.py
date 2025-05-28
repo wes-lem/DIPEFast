03302d203c83916dbeb6cace0d13d975bbd9c9e4
Weslem <weslem.wrs@gmail.com>
2025-05-28 16:58:48 -0300

    Database PyMySql

--- dao/database.py
+++ dao/database.py
@@ -1,24 +1,27 @@
+import pymysql
+pymysql.install_as_MySQLdb()
+
 import importlib
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Configuração do banco de dados MySQL
-DATABASE_URL = "mysql://root:ExbwVtQWJYneeCoMQqoJCymtxtZvDoIP@ballast.proxy.rlwy.net:59540/railway"
+# URL de conexão com o banco de dados (MySQL no Railway)
+DATABASE_URL = "mysql+pymysql://root:ExbwVtQWJYneeCoMQqoJCymtxtZvDoIP@ballast.proxy.rlwy.net:59540/railway"
 
-# Criação do engine do SQLAlchemy
+# Configuração do engine SQLAlchemy
+# connect_args adicionado para garantir charset no MySQL
 engine = create_engine(DATABASE_URL, connect_args={"charset": "utf8mb4"})
 
-# Sessão local para o banco de dados
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# Criação da sessão
+sessionlocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Base para os modelos
+# Base para os modelos declarativos
 Base = declarative_base()
-
 
 # Função para obter a sessão do banco
 def get_db():
-    db = SessionLocal()
+    db = sessionlocal()
     try:
         yield db
     finally:
